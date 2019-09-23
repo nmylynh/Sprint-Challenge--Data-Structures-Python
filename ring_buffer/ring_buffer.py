99dd4e65b3ce5528fffe1107c853f016c41ab3d6
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-09-23 05:10:55 -0700

    Update ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,5 @@
+# buffer: store (data) in a buffer while it is being processed or transferred.
+# A ring buffer is a buffer with a fixed size. When it fills up, adding another element overwrites the oldest one that was still being kept. It’s particularly useful for the storage of log information and history. 
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
